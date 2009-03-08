6491d4d807f13dfd5017763128c54ae0ff4fbb0e
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-07 16:58:24 -0800

    get and set are working

--- lib/remotehash.rb
+++ lib/remotehash.rb
@@ -1,34 +1,87 @@
 require 'uri'
 require 'xmlrpc/client'
 require 'digest/sha1'
+require 'logger'
 
 class RemoteHash
   VERSION = '1.0.0'
 
-  def initialize
-    uri = URI.parse("http://opendht.nyuld.net:5851/")
+  attr_reader :debug_output
+
+  def initialize secret = Time.now.to_f
+    uri = URI.parse("http://planetlab1.cs.uoregon.edu:5851/")
 
     @rpc = XMLRPC::Client.new3(
       :host => uri.host,
       :port => uri.port,
       :path => uri.path
     )
-    @secret = ''
+    @secret       = secret.to_s
+    self.debug_output = nil
+    self.debug_output = Logger.new($stdout) if $DEBUG
+  end
+
+  def debug_output= logger
+    @debug_output = logger
+    @rpc.instance_eval("@http").set_debug_output logger
   end
 
   def []= key, value
-    result = @rpc.call('put_removable',
-      XMLRPC::Base64.new(key),
-      XMLRPC::Base64.new(value),
-      'SHA',
-      XMLRPC::Base64.new(Digest::SHA1.digest(@secret)),
-      3600,
-      'remotehash'
-    )
-    redo if result == 2
-    raise if result == 1
+    retries = 0
+    begin
+      result = @rpc.call('put_removable',
+        XMLRPC::Base64.new(key),
+        XMLRPC::Base64.new(value),
+        'SHA',
+        XMLRPC::Base64.new(Digest::SHA1.digest(@secret)),
+        3600,
+        'remotehash'
+      )
+      raise if result == 1
+    rescue Timeout::Error => e
+      raise(e) if retries == 4
+      retries += 1
+      retry
+    end
   end
 
   def [] key
+    values = []
+    placemark = ''
+
+    D 'fetching key'
+    loop do
+      D 'calling get_details'
+      retries = 0
+      begin
+
+        result = @rpc.call('get_details',
+          XMLRPC::Base64.new(key),
+          10,
+          XMLRPC::Base64.new(placemark),
+          'remotehash'
+        )
+        values += result.first
+        placemark = result.last
+        break if placemark == ''
+
+      rescue Timeout::Error => e
+        raise(e) if retries == 4
+        retries += 1
+        retry
+      end
+    end
+
+    # Get the newest value with a matching sha1
+    (values.sort_by { |entry| entry[1] }.reverse.find { |entry|
+      entry.last == Digest::SHA1.digest(@secret)
+    } || []).first
+  end
+
+  private
+  def D message
+    return unless @debug_output
+    @debug_output << message
+    @debug_output << "\n"
   end
 end
--- test/test_remotehash.rb
+++ test/test_remotehash.rb
@@ -1,14 +1,29 @@
 require "test/unit"
 require "remotehash"
+require 'logger'
 
 class TestRemoteHash < Test::Unit::TestCase
   def test_new
     assert RemoteHash.new
   end
 
+  def test_debug_output=
+    assert hash = RemoteHash.new
+    hash.debug_output = Logger.new($stdout)
+  end
+
   def test_keys
     hash = RemoteHash.new
-    hash['foo'] = 'bar'
-    assert_equal 'bar', hash['foo']
+
+    hash['foo'] = 'baz'
+    assert_equal 'baz', hash['foo']
+  end
+
+  def test_two_instances
+    h1 = RemoteHash.new
+    h2 = RemoteHash.new
+
+    h1['foo'] = 'bar'
+    assert_nil h2['foo']
   end
 end
